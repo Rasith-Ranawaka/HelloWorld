e4992837fcce27aa7695ce99f127ef2448ccfbf5
Rasith-Ranawaka <rasithdr@gmail.com>
2018-01-14 13:35:02 +0530

    Another copy

---
+++ CopyOfhello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Test Repo</title>
+</head>
+
+    <body>
+    
+    <?php
+		echo "My name is rasith ranawaka";
+		
+	?>
+    </body>
+ 
+</html>
